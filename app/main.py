40a4e162389d8dbf9f45de1b0a3f695219e8f670
Matt Dies <matthewkdies@gmail.com>
2026-02-03 00:19:35 -0500

    Adding a validation exception handler.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 """The main processing entrypoint for the API. This module will start the API and handle immediate startup tasks."""
 
+import logging
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from app.api.v1 import author_router, quote_router
 from app.core.logging import setup_logging
@@ -11,6 +14,8 @@ from app.core.settings import settings
 from app.database import create_db_and_tables
 
 setup_logging()
+
+logger = logging.getLogger(__name__)
 
 API_PREFIX = "/api/v1"
 
@@ -27,3 +32,8 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None]:  # noqa: ARG001
 app = FastAPI(title=settings.app_name, lifespan=lifespan)
 app.include_router(author_router, prefix=API_PREFIX)
 app.include_router(quote_router, prefix=API_PREFIX)
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc) -> JSONResponse:  # noqa: ANN001, ARG001
+    """Logs the request validation exception to the console."""
+    logger.error("Validation Error: '%s'", exc.errors())
+    return JSONResponse(status_code=422, content={"detail": exc.errors()})
