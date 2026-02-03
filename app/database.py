2474b002e6b9c35baf84842e5d0276e450f209e6
Matt Dies <matthewkdies@gmail.com>
2026-02-02 23:58:44 -0500

    Creating a new Quote object, moving old object to SingleQuote, and adding ManyToMany relationship

--- app/database.py
+++ app/database.py
@@ -5,43 +5,91 @@ from typing import Annotated
 
 from fastapi import Depends
 from pydantic import computed_field
-from sqlmodel import Field, Session, SQLModel, create_engine
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine
 
 
 class Author(SQLModel, table=True):
-    """Defines a author."""
+    """Defines an author, who says a single quote."""
 
     id: int | None = Field(default=None, primary_key=True)
-    first_name: str = Field(index=True)
-    last_name: str
+    raw_name: str = Field(index=True, unique=True)  # will be firstname_lastname
 
     def __repr__(self):
         return (
-            f"{self.__class__.__name__}(id={self.id}, "
+            f"{self.__class__.__name__}"
+            f"(id={self.id}, "
+            f"raw_name={self.raw_name}, "
             f"first_name='{self.first_name}', "
             f"last_name='{self.last_name}', "
             f"name='{self.name}')"
         )
 
+    def __hash__(self) -> int:
+        return hash(self.raw_name)
+
+    @computed_field
+    @property
+    def first_name(self) -> str:
+        """Returns the author's first name."""
+        return self.raw_name.split("_")[0]
+
+    @computed_field
+    @property
+    def last_name(self) -> str:
+        """Returns the author's last name."""
+        return self.raw_name.split("_")[-1]
+
     @computed_field
     @property
     def name(self) -> str:
-        """Returns the author's full name, useful for display of a author within the templates."""
-        return f"{self.first_name} {self.last_name}"
+        """Returns the author's last name."""
+        return self.raw_name.replace("_", " ").title()
 
 
-class Quote(SQLModel, table=True):
-    """Defines a quote, which was said by a `Author`.
+class QuoteLink(SQLModel, table=True):
+    """Stores the IDs of a `SingleQuote` object and a `Quote` object, for linking them."""
 
-    Every quote is said by a `Author`, so
+    single_quote_id: int | None = Field(default=None, foreign_key="singlequote.id", primary_key=True)
+    quote_id: int | None = Field(default=None, foreign_key="quote.id", primary_key=True)
+
+
+class SingleQuote(SQLModel, table=True):
+    """Defines a single quote, which was said by a `Author`.
+
+    Every quote is said by a `Author`, so there's an `author_id` attribute linking back to the `Author` table.
+    Furthermore, a single quote may be a part of a larger quote, which can involve just one or multiple single quotes.
+    Accordingly, there is a `quote` attribute which links to the larger quote object.
     """
 
     def __repr__(self):
         return f"{self.__class__.__name__}(id={self.id}, author_id={self.author_id})"
 
     id: int | None = Field(default=None, primary_key=True)
-    quote: str
+    text: str
     author_id: int = Field(foreign_key="author.id")
+    # `Relationship` doesn't do anything in the database -- it just tells SQLModel/SQLAlchemy
+    # to fetch the list automatically when accessed. The `back_populates="single_quote"` tells
+    author: Author = Relationship()
+    quote: "Quote" = Relationship(
+        back_populates="single_quotes",
+        link_model=QuoteLink,
+    )  # link_model points to the junction table
+
+
+class Quote(SQLModel, table=True):
+    """A full quote, which is a collection of one or more single quotes.
+
+    One of these objects will link to at least one `SingleQuote` instance, but perhaps more.
+    This enables "conversational" quotes, in which there are multiple quotes from a single person.
+    For example: `"setup" - person_1; "punchline" - person_2`.
+
+    To do so, there's a `single_quotes: list[SingleQuote]` attribute, which links the tables.
+    """
+
+    id: int | None = Field(default=None, primary_key=True)
+    single_quotes: list[SingleQuote] = Relationship(back_populates="quote", link_model=QuoteLink)
+    before_context: str | None = Field(description="The context that sets the scene for the quote.")
+    after_context: str | None = Field(description="The context that for the quote, often as a punchline.")
 
 
 # TODO: configure how we actually connect to the database... likely Postgres, right?
