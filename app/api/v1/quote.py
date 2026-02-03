681be637fd36fc04e8adcfc52773044bc658aaf3
Matt Dies <matthewkdies@gmail.com>
2026-02-03 00:22:38 -0500

    Getting single quotes for quote.

--- app/api/v1/quote.py
+++ app/api/v1/quote.py
@@ -1,11 +1,18 @@
 """Defines endpoints related to interacting with the `Quote` object/table."""
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from sqlmodel import func, select
 
-from app.database import Author, Quote, SessionDep
+from app import TEMPLATES_DIR
+from app.api.v1.author import get_author_by_id
+from app.database import Author, Quote, SessionDep, SingleQuote
 
 quote_router = APIRouter()
+
+
+templates = Jinja2Templates(directory=TEMPLATES_DIR)
 
 
 # @quote_router.put("/quote", response_model=Quote)
@@ -45,6 +52,13 @@ def get_quote_by_id(quote_id: int, session: SessionDep) -> Quote:
     if not quote:
         raise HTTPException(status_code=404, detail=f"Quote with id={quote_id} not found.")
     return quote
+
+
+@quote_router.get("/quote/{quote_id}/single_quotes", response_model=list[SingleQuote])
+def get_quote_single_quotes(quote_id: int, session: SessionDep) -> list[SingleQuote]:
+    """Gets the `SingleQuote` objects that comprise a `Quote` object."""
+    quote: Quote = get_quote_by_id(quote_id, session)
+    return quote.single_quotes
 
 
 @quote_router.get("/quote/{quote_id}/author", response_model=Author)
